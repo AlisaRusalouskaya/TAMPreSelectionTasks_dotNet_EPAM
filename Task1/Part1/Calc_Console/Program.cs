ecf00e3113860085c9554d1195a813d629602daa
Alisa Rusalouskaya <alisa.rusalouskaya@thomsonreuters.com>
2017-04-13 09:18:39 +0300

    Task1 was added

---
+++ Task1/Part1/Calc_Console/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calc_Console
+{
+    class Calc_Area
+    {   public static double Calculator(string operation, double a, double b)
+        {
+            double result=0;
+            switch (operation)
+            {   
+                case "+":
+                    {
+                       return result = a + b;
+                        
+                    }
+                case "-":
+                    {
+                        return result = a - b;
+                        
+                    }
+                case "*":
+                    {
+                       return result = a * b;
+                       
+                    }
+                case "/":
+                    {
+                       return result = a / b;
+                        
+                    }
+                /*default: return System.Console.WriteLine("The operation is unavailable");
+                    break; */
+                }
+            return result;
+            
+        }
+
+    
+        static void Main()
+        {
+            string operation;
+            double result = 0;
+            System.Console.WriteLine("Enter the operation, please:");
+            operation = Console.ReadLine();
+            System.Console.WriteLine("Enter the first number, please:");
+            double a = Convert.ToDouble(System.Console.ReadLine());
+            System.Console.WriteLine("Enter the second number, please:");
+            double b = Convert.ToDouble(System.Console.ReadLine());
+            Calc_Area calculations = new Calc_Area();
+            result = Calc_Area.Calculator(operation, a, b);
+            System.Console.WriteLine("Result : {0}", result);
+            // Keep the console window open in debug mode.
+            System.Console.WriteLine("Press any key to exit.");
+            System.Console.ReadKey();
+        }
+    }
+        
+}
---
+++ Task1/Part2/Arrays_Matrix/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays_Matrix
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("The result of dimension of two matrix:");
+            double [,] my_arr = new double[,] {{ 2, 2, 3}, { 5, 2, 8}};
+            double[,] my_arr2 = new double[,] { { 2, 4, 8 }, { 1, 3, 1}};
+
+            var n = my_arr.GetLength(0);  /* number of strings*/
+            var m = my_arr.GetLength(1); /* number of columns */
+
+            Console.WriteLine("The first matrix is:");
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                    Console.Write("{0} ", my_arr[i, j]);
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("The second matrix is:");
+            for (int i = 0; i < my_arr2.GetLength(0); i++)
+            {
+                for (int j = 0; j < my_arr2.GetLength(1); j++)
+                    Console.Write("{0} ", my_arr2[i, j]);
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("The result is:");
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                   double [,] S = new double[n,m];
+                   S[i, j] = 0;
+                   for (int k = 0; k < n; k++)
+                   {
+                       S[i, j] += my_arr[i, k] * my_arr2[k, j];
+                       
+                   }
+                   Console.Write(S[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
